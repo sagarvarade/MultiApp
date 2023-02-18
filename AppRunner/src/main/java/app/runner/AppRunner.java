6f0588930d7f4db125c6288ae19c74b74400b722
sagar_varade <varade.sagar12@gmail.com>
2023-02-18 19:56:33 +0530

    Files updated

--- AppRunner/src/main/java/app/runner/AppRunner.java
+++ AppRunner/src/main/java/app/runner/AppRunner.java
@@ -1,21 +1,10 @@
 package app.runner;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import app.runner.MakeRunnable.MakeRunnable;
 
-/*public class AppRunner {
-	public static void main(String[] args) {
-		String[] ary= {"SAgar"};
-		SpringApp1.main(ary);
-		SpringApp2.main(ary);
-	}
-}*/
-
-@SpringBootApplication
-@ComponentScan(basePackages = { "com.SpringApp1.*", "com.SpringApp2.*" })
 public class AppRunner {
 	public static void main(String[] args) {
-		SpringApplication.run(AppRunner.class, args);
+		MakeRunnable.MakeRunnableFolder();
+		System.out.println("Done");
 	}
 }

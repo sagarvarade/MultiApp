41c8a0d1c81c56507f7540b2331fab155571ef43
sagar_varade <varade.sagar12@gmail.com>
2023-02-18 14:45:30 +0530

    Files updated

---
+++ SpringApp2/src/main/java/com/SpringApp2/SpringApp2.java
@@ -0,0 +1,13 @@
+package com.SpringApp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class SpringApp2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApp2.class, args);
+	}
+}
